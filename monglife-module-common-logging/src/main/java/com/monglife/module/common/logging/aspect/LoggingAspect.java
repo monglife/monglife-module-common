e495d5fa2713d8007baa64ade63d3c13b632721a
tableminpark <tablemin_park@daum.net>
2025-06-02 18:00:49 +0900

    fix: logging 모듈 수정

--- monglife-module-common-logging/src/main/java/com/monglife/module/common/logging/aspect/LoggingAspect.java
+++ monglife-module-common-logging/src/main/java/com/monglife/module/common/logging/aspect/LoggingAspect.java
@@ -1,23 +1,27 @@
 package com.monglife.module.common.logging.aspect;
 
-import com.monglife.module.common.logging.utils.ArgsUtil;
 import com.monglife.core.exception.ErrorException;
+import com.monglife.module.common.logging.utils.ArgsUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.lang.reflect.Method;
+import java.util.UUID;
 
 @Slf4j
 @Aspect
 @Component
 public class LoggingAspect {
+
+    @Value("${spring.config.activate.on-profile}")
+    private String profile;
 
     @Pointcut("execution(* com.monglife..*Consumer.*(..))")
     private void consumerPointcut() {}
@@ -25,24 +29,52 @@ public class LoggingAspect {
     @Pointcut("execution(* com.monglife..*Controller.*(..))")
     private void controllerPointcut() {}
 
-    @Pointcut("execution(* com.monglife..*Service.*(..))")
-    private void servicePointcut() {}
+    @Pointcut("execution(* com.monglife..*Worker.*(..))")
+    private void workerPointcut() {}
 
     @Pointcut("execution(* com.monglife..*Listener.*(..))")
     private void listenerPointcut() {}
 
+    @Pointcut("execution(* com.monglife..*UseCase.*(..))")
+    private void useCasePointcut() {}
+
+    @Pointcut("execution(* com.monglife..*Service.*(..))")
+    private void servicePointcut() {}
+
+    @Pointcut("execution(* com.monglife..*Port.*(..))")
+    private void portPointcut() {}
+
     @Pointcut("execution(* com.monglife..*Repository.*(..))")
     private void repositoryPointcut() {}
 
-    @Pointcut("consumerPointcut() || controllerPointcut() || servicePointcut() || listenerPointcut()")
+    @Pointcut("consumerPointcut() || controllerPointcut() || listenerPointcut()")
+    private void endPointPointcut() {}
+
+    @Pointcut("consumerPointcut() || controllerPointcut() || listenerPointcut() || useCasePointcut() || servicePointcut() || portPointcut() || repositoryPointcut()")
     private void targetPointcut() {}
+
+    @Around("endPointPointcut()")
+    public Object aroundEndPoint(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        if (MDC.get("traceId") == null || MDC.get("traceId").isBlank()) {
+            MDC.put("traceId", UUID.randomUUID().toString());
+        }
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            MDC.clear();
+        }
+    }
 
     /**
      * None Transactional 메서드 로깅 함수
      * @param joinPoint 조인 포인트
      */
     @Before("targetPointcut() && !@annotation(org.springframework.transaction.annotation.Transactional) && !@annotation(com.monglife.module.common.logging.annotation.DisableLogging)")
-    public void around(JoinPoint joinPoint) {
+    public void beforeNoTransactional(JoinPoint joinPoint) {
+
+        String traceId = MDC.get("traceId");
 
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
@@ -50,7 +82,11 @@ public class LoggingAspect {
         String clazzName = method.getDeclaringClass().getName();
         String methodName = method.getName();
 
-        log.debug("\n[METHOD INVOKE] < X > {}#{} {}", clazzName, methodName, ArgsUtil.generateArgs(method, joinPoint.getArgs()));
+        if (profile != null && !profile.isBlank() && ("dev".equals(profile) || "stg".equals(profile))) {
+            log.info("[{}] <TRANSACTION: X> {}#{} {}", traceId, clazzName, methodName, ArgsUtil.generateArgs(method, joinPoint.getArgs()));
+        } else {
+            log.debug("[{}] <TRANSACTION: X> {}#{} {}", traceId, clazzName, methodName, ArgsUtil.generateArgs(method, joinPoint.getArgs()));
+        }
     }
 
     /**
@@ -60,13 +96,19 @@ public class LoggingAspect {
     @Before("targetPointcut() && @annotation(org.springframework.transaction.annotation.Transactional) && !@annotation(com.monglife.module.common.logging.annotation.DisableLogging)")
     public void beforeTransactional(JoinPoint joinPoint) {
 
+        String traceId = MDC.get("traceId");
+
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
 
         String clazzName = method.getDeclaringClass().getName();
         String methodName = method.getName();
 
-        log.debug("\n[METHOD INVOKE] <{}> {}#{} {}", TransactionSynchronizationManager.getCurrentTransactionName(), clazzName, methodName, ArgsUtil.generateArgs(method, joinPoint.getArgs()));
+        if (profile != null && !profile.isBlank() && ("dev".equals(profile) || "stg".equals(profile))) {
+            log.info("[{}] <TRANSACTION: {}> {}#{} {}", traceId, TransactionSynchronizationManager.getCurrentTransactionName(), clazzName, methodName, ArgsUtil.generateArgs(method, joinPoint.getArgs()));
+        } else {
+            log.debug("[{}] <TRANSACTION: {}> {}#{} {}", traceId, TransactionSynchronizationManager.getCurrentTransactionName(), clazzName, methodName, ArgsUtil.generateArgs(method, joinPoint.getArgs()));
+        }
     }
 
     /**
@@ -74,8 +116,10 @@ public class LoggingAspect {
      * @param joinPoint 조인 포인트
      * @param exception 발생 예외
      */
-    @AfterThrowing(value = "controllerPointcut() || consumerPointcut() || listenerPointcut()", throwing = "exception")
+    @AfterThrowing(value = "targetPointcut()", throwing = "exception")
     public void afterThrowingException(JoinPoint joinPoint, Exception exception) {
+
+        String traceId = MDC.get("traceId");
 
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
@@ -89,7 +133,6 @@ public class LoggingAspect {
             message = errorException.getErrorCode() == null ? "" : errorException.getErrorCode().getMessage();
         }
 
-        log.error("\n[THROW] {}#{}\n{} : {}", clazzName, methodName, exception, message);
+        log.error("[{}] <THROW> {}#{} {} => {}\n{}", traceId, clazzName, methodName, exception, message, exception.getStackTrace());
     }
-
 }
--- monglife-module-common-security/src/main/java/com/monglife/module/common/security/response/SecurityResponse.java
+++ monglife-module-common-security/src/main/java/com/monglife/module/common/security/response/SecurityResponse.java
@@ -13,8 +13,8 @@ import java.util.Map;
 @AllArgsConstructor
 public enum SecurityResponse implements Response {
 
-    SECURITY_UNAUTHORIZED(HttpStatus.UNAUTHORIZED.value(), "SECURITY-100", "인증에 실패했습니다."),
-    SECURITY_FORBIDDEN(HttpStatus.FORBIDDEN.value(), "SECURITY-101", "권한이 없습니다.");
+    SECURITY_UNAUTHORIZED(HttpStatus.UNAUTHORIZED.value(), "000-000-100", "인증에 실패했습니다."),
+    SECURITY_FORBIDDEN(HttpStatus.FORBIDDEN.value(), "000-000-101", "권한이 없습니다.");
 
     private final Integer httpStatus;
     private final String code;
