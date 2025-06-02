661e3256c2afe0978524d6aef02e6967eafef425
박상민 <tablemin_park@daum.net>
2025-06-02 18:16:00 +0900

    logging 모듈 수정 (#15)

--- monglife-module-common-kafka/src/main/java/com/monglife/module/common/kafka/service/KafkaService.java
+++ monglife-module-common-kafka/src/main/java/com/monglife/module/common/kafka/service/KafkaService.java
@@ -2,6 +2,7 @@ package com.monglife.module.common.kafka.service;
 
 import com.monglife.module.common.kafka.event.TransactionEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,9 @@ public class KafkaService {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
+    @Value("${spring.config.activate.on-profile}")
+    private String profile;
+
     /**
      * 이벤트 발생
      * @param topic 이벤트 토픽 (주제)
@@ -18,6 +22,21 @@ public class KafkaService {
      */
     public <T> void generateEvent(String topic, T data) {
         kafkaTemplate.send(topic, TransactionEvent.builder()
+                .topic(topic)
+                .data(data)
+                .build());
+    }
+
+    /**
+     * 이벤트 발생
+     * @param topic 이벤트 토픽 (주제)
+     * @param data 이벤트 본문
+     */
+    public <T> void generateEventWithProfile(String topic, T data) {
+
+        String topicWithProfile = (profile != null && !profile.isBlank() ? profile + "." : "unknown.") + topic;
+
+        kafkaTemplate.send(topicWithProfile, TransactionEvent.builder()
                 .topic(topic)
                 .data(data)
                 .build());
