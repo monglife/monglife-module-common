3d8ca83f821372a9f1f04180471981908f685d0c
tableminpark <tablemin_park@daum.net>
2025-06-03 19:03:33 +0900

    Release v1.0.31

--- monglife-module-common-logging/src/main/java/com/monglife/module/common/logging/utils/ArgsUtil.java
+++ monglife-module-common-logging/src/main/java/com/monglife/module/common/logging/utils/ArgsUtil.java
@@ -1,11 +1,32 @@
 package com.monglife.module.common.logging.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ArgsUtil {
+
+    private static final ObjectMapper objectMapper;
+
+    static {
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        Hibernate6Module hibernate6Module = new Hibernate6Module();
+        hibernate6Module.disable(Hibernate6Module.Feature.FORCE_LAZY_LOADING);
+
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(javaTimeModule);
+        objectMapper.registerModule(hibernate6Module);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_SELF_REFERENCES, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS, false);
+
+    }
 
     /**
      * 로깅을 위한 메서드 파라미터 맵 생성
@@ -19,10 +40,15 @@ public class ArgsUtil {
             String parameterName = parameters[index].getName();
             Object arg = args[index];
 
-            if (arg == null || cantObjectPackage(arg)) {
-                argsMap.put(parameterName, "");
-            } else {
-                argsMap.put(parameterName, arg);
+            try {
+                if (arg == null || cantObjectPackage(arg)) {
+                    argsMap.put(parameterName, "");
+                } else {
+                    objectMapper.writeValueAsString(arg);
+                    argsMap.put(parameterName, arg);
+                }
+            } catch (Exception e) {
+                argsMap.put(parameterName, "arg parse failed.");
             }
         }
 
