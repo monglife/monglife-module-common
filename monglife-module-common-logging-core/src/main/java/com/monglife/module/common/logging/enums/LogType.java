d111b9b8cee148709eaae25568fd93bee9a7551f
tableminpark <tablemin_park@daum.net>
2025-06-05 17:24:34 +0900

    Release v2.0.4

--- monglife-module-common-logging-core/src/main/java/com/monglife/module/common/logging/enums/LogType.java
+++ monglife-module-common-logging-core/src/main/java/com/monglife/module/common/logging/enums/LogType.java
@@ -1,7 +1,7 @@
 package com.monglife.module.common.logging.enums;
 
 public enum LogType {
-
+    INITIALIZE,
     EXCEPTION,
     METHOD_CALL,
     METHOD_RETURN,
--- monglife-module-common-logging/src/main/java/com/monglife/module/common/logging/pointcut/LoggingPointcut.java
+++ monglife-module-common-logging/src/main/java/com/monglife/module/common/logging/pointcut/LoggingPointcut.java
@@ -4,36 +4,27 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class LoggingPointcut {
     
-    @Pointcut("execution(* com.monglife..*Consumer.*(..))")
+    @Pointcut("execution(* com.monglife..consumer.*Consumer.*(..))")
     private void consumerPointcut() {}
 
-    @Pointcut("execution(* com.monglife..*Controller.*(..))")
+    @Pointcut("execution(* com.monglife..controller.*Controller.*(..))")
     private void controllerPointcut() {}
 
-    @Pointcut("execution(* com.monglife..*Worker.*(..))")
+    @Pointcut("execution(* com.monglife..worker.*Worker.*(..))")
     private void workerPointcut() {}
 
-    @Pointcut("execution(* com.monglife..*Initializer.*(..))")
+    @Pointcut("execution(* com.monglife..initializer.*Initializer.*(..))")
     private void initializerPointcut() {}
 
-    @Pointcut("execution(* com.monglife..*UseCase.*(..))")
-    private void useCasePointcut() {}
-
-    @Pointcut("execution(* com.monglife..*Service.*(..))")
+    @Pointcut("execution(* com.monglife..service.*Service.*(..))")
     private void servicePointcut() {}
 
-    @Pointcut("execution(public * com.monglife..model.*.*(..))")
-    private void domainPointcut() {}
-
-    @Pointcut("execution(* com.monglife..*Port.*(..))")
-    private void portPointcut() {}
-
-    @Pointcut("execution(* com.monglife..*Listener.*(..))")
+    @Pointcut("execution(* com.monglife..listener.*Listener.*(..))")
     private void listenerPointcut() {}
 
-    @Pointcut("execution(* com.monglife..*Repository.*(..))")
+    @Pointcut("execution(* com.monglife..repository..*Repository*.*(..))")
     private void repositoryPointcut() {}
 
-    @Pointcut("consumerPointcut() || controllerPointcut() || workerPointcut() || initializerPointcut() || useCasePointcut() || servicePointcut() || domainPointcut() || listenerPointcut() || portPointcut() || repositoryPointcut()")
+    @Pointcut("consumerPointcut() || controllerPointcut() || workerPointcut() || initializerPointcut() || servicePointcut() || listenerPointcut() || repositoryPointcut()")
     public void allPointcut() {}
 }
