8d7996f8c97ec7999c4acc139e9d9ad8f9e3de70
tableminpark <tablemin_park@daum.net>
2025-06-10 11:14:53 +0900

    Release v2.0.9

--- monglife-module-common-logging/src/main/java/com/monglife/module/common/logging/config/LoggingAutoConfig.java
+++ monglife-module-common-logging/src/main/java/com/monglife/module/common/logging/config/LoggingAutoConfig.java
@@ -1,12 +1,11 @@
 package com.monglife.module.common.logging.config;
 
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
@@ -17,30 +16,19 @@ public class LoggingAutoConfig {
      * 로깅용 objectMapper
      */
     @Bean("LoggingObjectMapper")
-    @ConditionalOnClass(name = "com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module")
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         JavaTimeModule javaTimeModule = new JavaTimeModule();
-        Hibernate6Module hibernate6Module = new Hibernate6Module();
-        hibernate6Module.disable(Hibernate6Module.Feature.FORCE_LAZY_LOADING);
 
-        objectMapper.registerModule(javaTimeModule);
-        objectMapper.registerModule(hibernate6Module);
-        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        objectMapper.configure(SerializationFeature.FAIL_ON_SELF_REFERENCES, false);
-        objectMapper.configure(SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS, false);
-
-        return objectMapper;
-    }
-
-    /**
-     * Hibernate 제외 로깅 ObjectMapper
-     */
-    @Bean("LoggingObjectMapper")
-    @ConditionalOnMissingBean(name = "LoggingObjectMapper")
-    public ObjectMapper objectMapperWithoutHibernate() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        try {
+            Class<?> clazz = Class.forName("com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module");
+            Object hibernate6Module = clazz.getDeclaredConstructor().newInstance();
+            clazz.getMethod("disable", Enum.class).invoke(hibernate6Module, Enum.valueOf((Class<Enum>) Class.forName("com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module$Feature"),"FORCE_LAZY_LOADING"));
+            objectMapper.registerModule((Module) hibernate6Module);
+        } catch (ClassNotFoundException ignored) {
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         objectMapper.registerModule(javaTimeModule);
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
